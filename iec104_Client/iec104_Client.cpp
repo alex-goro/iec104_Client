309bca737df188b29719b8150c9d8f8fa1ca1031
alex-goro <gorokhov_alex@mail.ru>
2015-02-07 17:30:30 +0300

    First commit

---
+++ iec104_Client/iec104_Client.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include <WinSock2.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+	cout << "Hello Worlld" << endl;
+	getchar();    
+}
